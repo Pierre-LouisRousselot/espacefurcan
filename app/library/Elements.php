ed3452d261c02752f74ede9912dadc1efbee1884
Rousselot Pierre-Louis <pierrelouis.rousselot@viacesi.fr>
2018-11-14 13:24:44 +0100

    Ajout de la nav bar

--- app/library/Elements.php
+++ app/library/Elements.php
@@ -3,12 +3,133 @@
 use Phalcon\Mvc\User\Component;
 
 /**
-* Elements
-*
-* Helps to build UI elements for the application
-*/
+ * Elements
+ *
+ * Helps to build UI elements for the application
+ */
 class Elements extends Component
 {
+    private $_headerMenu = [
+        'navbar-left' => [
+            'index' => [
+                'caption' => 'Accueil',
+                'action' => 'index'
+            ],
+            'services' => [
+                'caption' => 'Services',
+                'action' => 'index'
+            ],
+            'informatique' => [
+                'caption' => 'Informatique',
+                'action' => 'index'
+            ],
+            'formations' => [
+                'caption' => 'Formations',
+                'action' => 'index'
+            ],
+            'boutique' => [
+                'caption' => 'Boutique',
+                'action' => 'index'
+            ],
+            'about' => [
+                'caption' => 'A propos',
+                'action' => 'index'
+            ],
+            'contact' => [
+                'caption' => 'Nous contacter',
+                'action' => 'index'
+            ],
+        ],
+        'navbar-right' => [
+            'session' => [
+                'caption' => 'Se connecter',
+                'action' => 'index'
+            ],
+        ]
+    ];
 
+    private $_tabs = [
+        'Invoices' => [
+            'controller' => 'invoices',
+            'action' => 'index',
+            'any' => false
+        ],
+        'Companies' => [
+            'controller' => 'companies',
+            'action' => 'index',
+            'any' => true
+        ],
+        'Products' => [
+            'controller' => 'products',
+            'action' => 'index',
+            'any' => true
+        ],
+        'Product Types' => [
+            'controller' => 'producttypes',
+            'action' => 'index',
+            'any' => true
+        ],
+        'Your Profile' => [
+            'controller' => 'invoices',
+            'action' => 'profile',
+            'any' => false
+        ]
+    ];
 
+    /**
+     * Builds header menu with left and right items
+     *
+     * @return string
+     */
+    public function getMenu()
+    {
+
+        $auth = $this->session->get('auth');
+        if ($auth) {
+            $this->_headerMenu['navbar-right']['session'] = [
+                'caption' => 'Se déconnecter',
+                'action' => 'end'
+            ];
+        } else {
+            unset($this->_headerMenu['navbar-left']['invoices']);
+        }
+
+        $controllerName = $this->view->getControllerName();
+        foreach ($this->_headerMenu as $position => $menu) {
+            echo '<div class="nav-collapse">';
+            echo '<ul class="nav navbar-nav ', $position, '">';
+            foreach ($menu as $controller => $option) {
+                if ($controllerName == $controller) {
+                    echo '<li class="active">';
+                } else {
+                    echo '<li>';
+                }
+                echo $this->tag->linkTo($controller . '/' . $option['action'], $option['caption']);
+                echo '</li>';
+            }
+            
+            echo '</ul>';
+            echo '</div>';
+        }
+
+    }
+
+    /**
+     * Returns menu tabs
+     */
+    public function getTabs()
+    {
+        $controllerName = $this->view->getControllerName();
+        $actionName = $this->view->getActionName();
+        echo '<ul class="nav nav-tabs">';
+        foreach ($this->_tabs as $caption => $option) {
+            if ($option['controller'] == $controllerName && ($option['action'] == $actionName || $option['any'])) {
+                echo '<li class="active">';
+            } else {
+                echo '<li>';
+            }
+            echo $this->tag->linkTo($option['controller'] . '/' . $option['action'], $caption), '</li>';
+        }
+        echo '</ul>';
+    }
 }
