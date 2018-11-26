bbf2f025322011da72279a4023846143d82dab1d
Aharrar Ibrahim <tanger1040@hotmail.com>
2018-11-26 17:52:35 +0100

    WIP tableau,fonction article

--- app/plugins/SecurityPlugin.php
+++ app/plugins/SecurityPlugin.php
@@ -52,7 +52,7 @@ class SecurityPlugin extends Plugin
 
 				//Private area resources
 				$privateResources = [
-					'admin'        => ['index'],
+					'admin'        => ['index','showProduct'],
 					'services'     => ['index'],
 					'informatique' => ['index'],
 				];
