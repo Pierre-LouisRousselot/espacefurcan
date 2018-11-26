9a3d12a9f4b937d2e98b0ac55c4454679bc1c8a4
KevinCoujandassamy <kevin@KEVIN-PC.localdomain>
2018-11-26 17:54:08 +0100

    ADD files gestion des pages

---
+++ app/models/CatePages.php
@@ -0,0 +1,17 @@
+<?php
+
+use Phalcon\Mvc\Model;
+
+class Catepages extends Model
+{
+    /**
+     * @var integer
+     */
+    public $id_CatePage;
+
+    /**
+     * @var string
+     */
+    public $nom_CatePage;
+
+}
---
+++ app/views/pages/index.volt
@@ -0,0 +1,2 @@
+{{ content() }}
+Gestion des pages
