e1365fd979ef25da0c6da0fcd2d3575fbd6c3531
KevinCoujandassamy <kevin@KEVIN-PC.localdomain>
2018-11-27 17:56:03 +0100

    FIX conflict

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -27,11 +27,11 @@ class AdminController extends ControllerBase
     public function showProductAction(){
 
          $produits = Produits::find();
-         
+
          // foreach ($produits as $produit) {
          //     // var_dump($produit->id_Produit);
             $this->view->produits = $produits;
- 
+
         if (!$this->request->isPost()) {
             $this->tag->setDefault('id', $produits->id_Produit);
             $this->tag->setDefault('name', $produits->nom_Produit);
