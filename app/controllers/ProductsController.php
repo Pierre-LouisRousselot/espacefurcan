646cc7be7e50a9ea5c2a399f2c3f6c33e8c573bb
Aharrar Ibrahim <tanger1040@hotmail.com>
2018-11-27 17:03:09 +0100

    WIP bouton delete pas fini

--- app/controllers/ProductsController.php
+++ app/controllers/ProductsController.php
@@ -1,5 +1,8 @@
 <?php
 
+use Phalcon\Paginator\Adapter\Model as Paginator;
+
+
 class ProductsController extends ControllerBase
 {
     public function initialize()
@@ -7,6 +10,48 @@ class ProductsController extends ControllerBase
         $this->tag->setTitle('Bienvenue');
         parent::initialize();
     }
+
+    // public function paginationAction()
+    // {
+        
+    //     $this->assets->addCss("css/global.css");
+
+    //     $numberPage = 1;
+    //     if ($this->request->isPost()) {
+    //         $query = Criteria::fromInput($this->di,
+    //         	"produits", $this->request->getPost());
+    //         $this->persistent->searchParams = $query->getParams();
+    //     } else {
+    //         $numberPage = $this->request->getQuery("page", "int");
+    //     }
+
+    //     $parameters = [];
+    //     if ($this->persistent->searchParams) {
+    //         $parameters = $this->persistent->searchParams;
+    //     }
+
+    //     $produits = Products::find($parameters);
+    //     if (count($produits) == 0) {
+    //         $this->flash->notice("The search did not find any users");
+
+    //         return $this->dispatcher->forward(
+    //             [
+    //                 "controller" => "produits",
+    //                 "action"     => "index",
+    //             ]
+    //         );
+    //     }
+
+    //     $paginator = new Paginator([
+    //         "data"  => $produits,
+    //         "limit" => 10,
+    //         "page"  => $numberPage
+    //     ]);
+
+    //     $this->view->page = $paginator->getPaginate();
+    //     $this->view->produits = $produits;
+
+    // }
 
     public function indexAction()
     {
@@ -55,5 +100,43 @@ class ProductsController extends ControllerBase
           $this->view->form = $form;
     }
 
+     public function deleteAction($id)
+    {
+var_dump($test);die();
+        $produits = Produits::findFirstById($id);
+        //var_dump($produits);die();
+        if (!$produits) {
+            $this->flash->error("User was not found");
+
+            return $this->dispatcher->forward(
+                [
+                    "controller" => "products",
+                    "action"     => "index",
+                ]
+            );
+        }
+
+        if (!$produits->delete()) {
+            foreach ($produits->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            return $this->dispatcher->forward(
+                [
+                    "controller" => "products",
+                    "action"     => "search",
+                ]
+            );
+        }
+
+        $this->flash->success("Le produit a été supprimer");
+
+        return $this->dispatcher->forward(
+            [
+                "controller" => "products",
+                "action"     => "index",
+            ]
+        );
+    }
 
 }
