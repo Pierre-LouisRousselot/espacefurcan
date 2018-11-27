efa78fe784254636b51908dc0aa5c58079aab79d
Pierre-LouisRousselot <pierrelouis.rousselot@viacesi.fr>
2018-11-27 17:42:39 +0100

    WIP form edit user

---
+++ app/forms/UsersForm.php
@@ -0,0 +1,60 @@
+<?php
+
+use Phalcon\Forms\Form;
+use Phalcon\Forms\Element\Text;
+use Phalcon\Forms\Element\Password;
+use Phalcon\Validation\Validator\PresenceOf;
+use Phalcon\Validation\Validator\Email;
+
+class UsersForm extends Form
+{
+    public function initialize($entity = null, $options = null)
+    {
+        // Name
+        $name = new Text('nom');
+        $name->setLabel('Votre nom complet');
+        $name->setFilters(['striptags', 'string']);
+
+        $this->add($name);
+
+        // Name
+        $name = new Text('prenom');
+        $name->setLabel('Prénom');
+        $name->setFilters(['alphanum']);
+
+        $this->add($name);
+
+        // Email
+        $email = new Text('email');
+        $email->setLabel('E-Mail');
+        $email->setFilters('email');
+        $email->addValidators([
+            new Email([
+                'message' => 'Adresse mail invalide'
+            ])
+        ]);
+        $this->add($email);
+
+
+        // Téléphone
+        $telephone = new Text('telephone');
+        $telephone->setLabel('Téléphone');
+        $this->add($telephone);
+
+        // Adresse
+        $adresse = new Text('adresse');
+        $adresse->setLabel('Adresse');
+        $this->add($adresse);
+
+        // Ville
+        $ville = new Text('ville');
+        $ville->setLabel('Ville');
+        $this->add($ville);
+
+        // Code postal
+        $postal = new Text('postal');
+        $postal->setLabel('Code postal');
+        $this->add($postal);
+
+    }
+}
