ac23d821356975df98ab606e65e402ed5443be9a
KevinCoujandassamy <kevin.coujandassamy@gmail.com>
2018-12-13 14:36:02 +0100

    UPDATE cms js

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -3,222 +3,222 @@ use Phalcon\Mvc\Url;
 
 class PagesController extends ControllerBase
 {
-    public function initialize()
-    {
-        $this->tag->setTitle('Gestion des pages');
-        parent::initialize();
-    }
+  public function initialize()
+  {
+    $this->tag->setTitle('Gestion des pages');
+    parent::initialize();
+  }
 
-    /**
-     * Permet d'ajouter la fonction de Dropdown et de lancer le formulaire
-     */
-    public function indexAction()
-    {
-        //$this->assets->addJs('//cloud.tinymce.com/stable/tinymce.min.js', false);
-        $this->addDrop();
+  /**
+  * Permet d'ajouter la fonction de Dropdown et de lancer le formulaire
+  */
+  public function indexAction()
+  {
+    //$this->assets->addJs('//cloud.tinymce.com/stable/tinymce.min.js', false);
+    $this->addDrop();
 
-        $form = new PageForm;
+    $form = new PageForm;
 
-        $this->view->form = $form;
+    $this->view->form = $form;
 
 
-    }
+  }
 
-    /**
-     * Permet d'afficher les pages statiques
-     * @param  int  $id [id de la page à afficher]
-     * @return [string]     [contenu de la page]
-     */
-    public function displayPageAction($id)
-    {
+  /**
+  * Permet d'afficher les pages statiques
+  * @param  int  $id [id de la page à afficher]
+  * @return [string]     [contenu de la page]
+  */
+  public function displayPageAction($id)
+  {
 
-        $content = Pages::find([
-            "conditions" => "id_Page = ". $id
+    $content = Pages::find([
+      "conditions" => "id_Page = ". $id
+    ]);
+
+    //Permet de formater les chemins d'images pour les afficher
+    $newContent = str_replace('<img src="', '<img src="../', $content[0]->contenu_Page);
+
+    $this->view->page = $newContent;
+  }
+
+  /**
+  * Permet d'afficher la page à modifier dans le CMS
+  * @param  int $id id de la page à charger dans le CMS
+  * @return JSON     [Json contenant les info de la page]
+  */
+  public function loadTinyAction($id)
+  {
+    $content = Pages::find([
+      "conditions" => "id_Page = ". $id
+    ]);
+    $pageContent = [
+      "titre" => $content[0]->titre_Page,
+      "content" => $content[0]->contenu_Page,
+      "cate" => $content[0]->id_CatePage
+    ];
+    //var_dump($pageContent);die;
+    return json_encode($pageContent);
+  }
+
+  /**
+  * Récupere les données du Dropdown
+  */
+  private function addDrop(){
+    $drop ='';
+    $catpage = CatePages::find();
+    foreach($catpage as $idService => $detailService){
+      if($detailService->id_CatePage != 3){
+        $drop .='<h5 class="dropdown-header">'.$detailService->nom_CatePage.'</h5>';
+        $page = Pages::find([
+          "conditions" => "id_CatePage =" .$detailService->id_CatePage
         ]);
-
-        //Permet de formater les chemins d'images pour les afficher
-        $newContent = str_replace('<img src="', '<img src="../', $content[0]->contenu_Page);
-
-        $this->view->page = $newContent;
-    }
-
-    /**
-     * Permet d'afficher la page à modifier dans le CMS
-     * @param  int $id id de la page à charger dans le CMS
-     * @return JSON     [Json contenant les info de la page]
-     */
-    public function loadTinyAction($id)
-    {
-        $content = Pages::find([
-            "conditions" => "id_Page = ". $id
+        foreach ($page as $idPage => $value) {
+          $drop .='<a class="dropdown-item" onclick="loadPage('.$value->id_Page.')" href="#">'.$value->titre_Page.'</a>';
+        }
+      } else {
+        $drop .='<div class="dropdown-divider"></div>';
+        $page = Pages::find([
+          "conditions" => "id_CatePage =" .$detailService->id_CatePage
         ]);
-        $pageContent = [
-            "titre" => $content[0]->titre_Page,
-            "content" => $content[0]->contenu_Page,
-            "cate" => $content[0]->id_CatePage
-        ];
-        //var_dump($pageContent);die;
-        return json_encode($pageContent);
+        $value = $page[0];
+        $drop .='<a class="dropdown-item" onclick="loadPage('.$value->id_Page.')" href="#">'.$value->titre_Page.'</a>';
       }
+    }
+    $this->view->drop = $drop;
+  }
 
-      /**
-      * Récupere les données du Dropdown
-      */
-      private function addDrop(){
-        $drop ='';
-        $catpage = CatePages::find();
-        foreach($catpage as $idService => $detailService){
-          if($detailService->id_CatePage != 3){
-            $drop .='<h5 class="dropdown-header">'.$detailService->nom_CatePage.'</h5>';
-            $page = Pages::find([
-              "conditions" => "id_CatePage =" .$detailService->id_CatePage
-            ]);
-            foreach ($page as $idPage => $value) {
-              $drop .='<a class="dropdown-item" onclick="loadPage('.$value->id_Page.')" href="#">'.$value->titre_Page.'</a>';
-            }
-          } else {
-            $drop .='<div class="dropdown-divider"></div>';
-            $page = Pages::find([
-              "conditions" => "id_CatePage =" .$detailService->id_CatePage
-            ]);
-            $value = $page[0];
-            $drop .='<a class="dropdown-item" onclick="loadPage('.$value->id_Page.')" href="#">'.$value->titre_Page.'</a>';
-          }
+  /**
+  * Permet de sauvegarder le contenu d'une page statique dans la bdd
+  */
+  public function savePageAction()
+  {
+
+    if( isset($_POST['id']) && isset($_POST['content']) && isset($_POST['title'])&& isset($_POST['idCate']) ){
+
+      $newpage = new Pages();
+      $newpage->id_Page = $_POST['id'];
+      $newpage->titre_Page = $_POST['title'];
+      $newpage->contenu_Page = $_POST['content'];
+      $newpage->dateUpdate_Page = new Phalcon\Db\RawValue('now()');
+      $newpage->id_CatePage = $_POST['idCate'];
+
+
+      if($newpage->save() == false){
+        foreach ($newpage->getMessages() as $message) {
+          $this->flash->error((string) $message);
         }
-        $this->view->drop = $drop;
       }
-
-      /**
-      * Permet de sauvegarder le contenu d'une page statique dans la bdd
-      */
-      public function savePageAction()
-      {
-
-        if( isset($_POST['id']) && isset($_POST['content']) && isset($_POST['title'])&& isset($_POST['idCate']) ){
-
-            $newpage = new Pages();
-            $newpage->id_Page = $_POST['id'];
-            $newpage->titre_Page = $_POST['title'];
-            $newpage->contenu_Page = $_POST['content'];
-            $newpage->dateUpdate_Page = new Phalcon\Db\RawValue('now()');
-            $newpage->id_CatePage = $_POST['idCate'];
-
-
-            if($newpage->save() == false){
-                foreach ($newpage->getMessages() as $message) {
-                    $this->flash->error((string) $message);
-                }
-            }
-        }
-
     }
 
-    /**
-     * Permet d'ajouter une page statique dans la bdd
-     */
-    public function addPageAction()
-    {
-        if( isset($_POST['content']) && isset($_POST['title'])&& isset($_POST['idCate']) ){
+  }
 
-            $newpage = new Pages();
-            $newpage->titre_Page = $_POST['title'];
-            $newpage->contenu_Page = $_POST['content'];
-            $newpage->dateUpdate_Page = new Phalcon\Db\RawValue('now()');
-            $newpage->id_CatePage = $_POST['idCate'];
+  /**
+  * Permet d'ajouter une page statique dans la bdd
+  */
+  public function addPageAction()
+  {
+    if( isset($_POST['content']) && isset($_POST['title'])&& isset($_POST['idCate']) ){
+
+      $newpage = new Pages();
+      $newpage->titre_Page = $_POST['title'];
+      $newpage->contenu_Page = $_POST['content'];
+      $newpage->dateUpdate_Page = new Phalcon\Db\RawValue('now()');
+      $newpage->id_CatePage = $_POST['idCate'];
 
 
-            if($newpage->create() == false){
-                foreach ($newpage->getMessages() as $message) {
-                    $this->flash->error((string) $message);
-                }
-            }
+      if($newpage->create() == false){
+        foreach ($newpage->getMessages() as $message) {
+          $this->flash->error((string) $message);
         }
+      }
     }
+  }
 
-    /**
-     * Permet de supprimer une page statique de la bdd
-     */
-    public function delPageAction()
-    {
-        if( isset($_GET['id'])){
+  /**
+  * Permet de supprimer une page statique de la bdd
+  */
+  public function delPageAction()
+  {
+    if( isset($_GET['id'])){
 
-            $content = Pages::find([
-                "conditions" => "id_Page = ". $_GET['id']
-            ]);
-            if($content->delete() == false){
-                foreach ($content->getMessages() as $message) {
-                    $this->flash->error((string) $message);
-                }
-            }
+      $content = Pages::find([
+        "conditions" => "id_Page = ". $_GET['id']
+      ]);
+      if($content->delete() == false){
+        foreach ($content->getMessages() as $message) {
+          $this->flash->error((string) $message);
         }
+      }
     }
+  }
 
-    /**
-     * Permet d'inclure des photos dans le CMS
-     * @return [JSON] [Retourne le chemin de l'image sauvegardée]
-     */
-    public function postAcceptorAction()
-    {
-        /*******************************************************
-         * Only these origins will be allowed to upload images *
-         ******************************************************/
-        $accepted_origins = array("http://localhost", "http://192.168.1.1", "http://espacefurcan.be");
+  /**
+  * Permet d'inclure des photos dans le CMS
+  * @return [JSON] [Retourne le chemin de l'image sauvegardée]
+  */
+  public function postAcceptorAction()
+  {
+    /*******************************************************
+    * Only these origins will be allowed to upload images *
+    ******************************************************/
+    $accepted_origins = array("http://localhost", "http://192.168.1.1", "http://espacefurcan.be");
 
-        /*********************************************
-        * Change this line to set the upload folder *
-        *********************************************/
+    /*********************************************
+    * Change this line to set the upload folder *
+    *********************************************/
 
-        $url = new Url();
-        $url->setBaseUri('');
-        $imageFolder = $url->getStatic('../public/images-page/');
+    $url = new Url();
+    $url->setBaseUri('');
+    $imageFolder = $url->getStatic('../public/images-page/');
 
-        reset ($_FILES);
-        $temp = current($_FILES);
+    reset ($_FILES);
+    $temp = current($_FILES);
 
-        if (is_uploaded_file($temp['tmp_name'])){
-          if (isset($_SERVER['HTTP_ORIGIN'])) {
-            // same-origin requests won't set an origin. If the origin is set, it must be valid.
-            if (in_array($_SERVER['HTTP_ORIGIN'], $accepted_origins)) {
-              header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-            } else {
-              header("HTTP/1.1 403 Origin Denied");
-              return;
-            }
-          }
-
-          /*
-            If your script needs to receive cookies, set images_upload_credentials : true in
-            the configuration and enable the following two headers.
-          */
-          // header('Access-Control-Allow-Credentials: true');
-          // header('P3P: CP="There is no P3P policy."');
-
-          // Sanitize input
-          if (preg_match("/([^\w\s\d\-_~,;:\[\]\(\).])|([\.]{2,})/", $temp['name'])) {
-              header("HTTP/1.1 400 Invalid file name.");
-              return;
-          }
-
-          // Verify extension
-          if (!in_array(strtolower(pathinfo($temp['name'], PATHINFO_EXTENSION)), array("gif", "jpg", "png"))) {
-              header("HTTP/1.1 400 Invalid extension.");
-              return;
-          }
-
-          // Accept upload if there was no origin, or if it is an accepted origin
-          $filetowrite = $imageFolder . $temp['name'];
-          $finalcheck = move_uploaded_file($temp['tmp_name'], $filetowrite);
-
-          // Respond to the successful upload with JSON.
-          // Use a location key to specify the path to the saved image resource.
-          // { location : '/your/uploaded/image/file'}
-
-          echo json_encode(array('location' => $filetowrite));
-          die;
+    if (is_uploaded_file($temp['tmp_name'])){
+      if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // same-origin requests won't set an origin. If the origin is set, it must be valid.
+        if (in_array($_SERVER['HTTP_ORIGIN'], $accepted_origins)) {
+          header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
         } else {
-          // Notify editor that the upload failed
-          header("HTTP/1.1 500 Server Error");
+          header("HTTP/1.1 403 Origin Denied");
+          return;
         }
+      }
+
+      /*
+      If your script needs to receive cookies, set images_upload_credentials : true in
+      the configuration and enable the following two headers.
+      */
+      // header('Access-Control-Allow-Credentials: true');
+      // header('P3P: CP="There is no P3P policy."');
+
+      // Sanitize input
+      if (preg_match("/([^\w\s\d\-_~,;:\[\]\(\).])|([\.]{2,})/", $temp['name'])) {
+        header("HTTP/1.1 400 Invalid file name.");
+        return;
+      }
+
+      // Verify extension
+      if (!in_array(strtolower(pathinfo($temp['name'], PATHINFO_EXTENSION)), array("gif", "jpg", "png"))) {
+        header("HTTP/1.1 400 Invalid extension.");
+        return;
+      }
+
+      // Accept upload if there was no origin, or if it is an accepted origin
+      $filetowrite = $imageFolder . $temp['name'];
+      $finalcheck = move_uploaded_file($temp['tmp_name'], $filetowrite);
+
+      // Respond to the successful upload with JSON.
+      // Use a location key to specify the path to the saved image resource.
+      // { location : '/your/uploaded/image/file'}
+
+      echo json_encode(array('location' => $filetowrite));
+      die;
+    } else {
+      // Notify editor that the upload failed
+      header("HTTP/1.1 500 Server Error");
     }
+  }
 
 }
--- public/js/page.js
+++ public/js/page.js
@@ -6,157 +6,160 @@ $("#idPage").val("");
 
 $(document).ready(function () {
   /**
-   * Initialisation du CMS tinyMCE
-   */
-    tinymce.init({
-        //content_css:"css/content.css"
-        selector:'textarea',
-        branding:'false',
-        plugins:"autoresize, image, table",
-        elementpath:'false',
-        language_url:'../public/js/fr_FR.js',
-        language:'fr_FR',
-        paste_data_images:'true',
-        images_upload_handler: function (blobInfo, success, failure) {
-            var xhr, formData;
+  * Initialisation du CMS tinyMCE
+  */
+  tinymce.init({
+    //content_css:"css/content.css"
+    selector:'textarea',
+    branding:'false',
+    plugins:"autoresize, image, table",
+    elementpath:'false',
+    language_url:'../public/js/fr_FR.js',
+    language:'fr_FR',
+    paste_data_images:'true',
+    images_upload_handler: function (blobInfo, success, failure) {
+      var xhr, formData;
 
-            xhr = new XMLHttpRequest();
-            xhr.withCredentials = false;
-            xhr.open('POST', 'postAcceptor');
+      xhr = new XMLHttpRequest();
+      xhr.withCredentials = false;
+      xhr.open('POST', 'postAcceptor');
 
-            xhr.onload = function() {
-                var json;
+      xhr.onload = function() {
+        var json;
 
-                if (xhr.status != 200) {
-                    failure('HTTP Error: ' + xhr.status);
-                    return;
-                }
-
-                json = JSON.parse(xhr.responseText);
-
-                if (!json || typeof json.location != 'string') {
-                    failure('Invalid JSON: ' + xhr.responseText);
-                    return;
-                }
-
-                success(json.location);
-            };
-
-            formData = new FormData();
-            formData.append('file', blobInfo.blob(), blobInfo.filename());
-
-            xhr.send(formData);
+        if (xhr.status != 200) {
+          failure('HTTP Error: ' + xhr.status);
+          return;
         }
-    });
+
+        json = JSON.parse(xhr.responseText);
+
+        if (!json || typeof json.location != 'string') {
+          failure('Invalid JSON: ' + xhr.responseText);
+          return;
+        }
+
+        success(json.location);
+      };
+
+      formData = new FormData();
+      formData.append('file', blobInfo.blob(), blobInfo.filename());
+
+      xhr.send(formData);
+    }
+  });
 });
 
 /**
- * Permet de charger une page dans le CMS TinyMCE
- * @param  {int} $id [id de la page à charger]
- */
+* Permet de charger une page dans le CMS TinyMCE
+* @param  {int} $id [id de la page à charger]
+*/
 function loadPage($id) {
   $("#titrePage").hide();
+  $("#dropdown").hide();
   $("#delPageButton").hide();
 
+  $.ajax({
+    url : 'loadTiny/'+$id,
+    type : 'GET',
+    success : function(data){
+      data = JSON.parse(data);
+      tinyMCE.activeEditor.setContent(data.content);
+      $("#titrePage").val(data.titre);
+      $("#idPage").val($id);
+      $("#idCatePage").val(data.cate),
+      $("#form").show();
+      if ($("#idCatePage").val() != 3) {
+        $("#titrePage").show();
+        $("#delPageButton").show();
+      }
+    },
+    error : function(resultat, statut, erreur){
+    }
+  });
+}
+
+/**
+* Permet de sauvegarder une page inscrite dans le form et le CMS
+*/
+function savePage() {
+  if ($("#idPage").val() != ""){
+    $dataPage = {
+      "id" : $("#idPage").val(),
+      "title" : $("#titrePage").val(),
+      "content" : tinyMCE.activeEditor.getContent(),
+      "idCate" : $("#idCatePage").val()
+    };
+
     $.ajax({
-        url : 'loadTiny/'+$id,
-        type : 'GET',
-        success : function(data){
-            data = JSON.parse(data);
-            tinyMCE.activeEditor.setContent(data.content);
-            $("#titrePage").val(data.titre);
-            $("#idPage").val($id);
-            $("#idCatePage").val(data.cate),
-            $("#form").show();
-            if ($("#idCatePage").val() != 3) {
-              $("#titrePage").show();
-              $("#delPageButton").show();
-            }
+      url : 'savePage',
+      type : 'POST',
+      data : $dataPage,
+      success : function(){
+        location.reload(true);
+      },
+      error : function(resultat, statut, erreur){
+      }
+    });
+  } else if ($("#idPage").val() == ""){
+    if ($("#dropdown").val() == 0) {
+      $("#warndrop").show();
+    }else {
+      $dataPage = {
+        "title" : $("#titrePage").val(),
+        "content" : tinyMCE.activeEditor.getContent(),
+        "idCate" : $("#dropdown").val()
+      };
+      $.ajax({
+        url : 'addPage',
+        type : 'POST',
+        data : $dataPage,
+        success : function(){
+          location.reload(true);
         },
         error : function(resultat, statut, erreur){
         }
-    });
-}
-
-/**
- * Permet de sauvegarder une page inscrite dans le form et le CMS
- */
-function savePage() {
-    if ($("#idPage").val() != ""){
-        $dataPage = {
-            "id" : $("#idPage").val(),
-            "title" : $("#titrePage").val(),
-            "content" : tinyMCE.activeEditor.getContent(),
-            "idCate" : $("#idCatePage").val()
-        };
-
-        $.ajax({
-            url : 'savePage',
-            type : 'POST',
-            data : $dataPage,
-            success : function(){
-                location.reload(true);
-            },
-            error : function(resultat, statut, erreur){
-            }
-        });
-    } else if ($("#idPage").val() == ""){
-        if ($("#dropdown").val() == 0) {
-            $("#warndrop").show();
-        }else {
-            $dataPage = {
-                "title" : $("#titrePage").val(),
-                "content" : tinyMCE.activeEditor.getContent(),
-                "idCate" : $("#dropdown").val()
-            };
-            $.ajax({
-                url : 'addPage',
-                type : 'POST',
-                data : $dataPage,
-                success : function(){
-                    location.reload(true);
-                },
-                error : function(resultat, statut, erreur){
-                }
-            });
-        }
+      });
     }
+  }
 }
 
 /**
- * Permet de réveler les elements nécessaire à l'ajout de page
- */
+* Permet de réveler les elements nécessaire à l'ajout de page
+*/
 function addPage() {
-    $("#dropdown").show();
-    tinyMCE.activeEditor.setContent("");
-    $("#titrePage").val("");
-    $("#titrePage").show();
-    $("#form").show();
-    $("#idPage").val("");
+  $("#dropdown").show();
+  tinyMCE.activeEditor.setContent("");
+  $("#titrePage").val("");
+  $("#titrePage").show();
+  $("#form").show();
+  $("#idPage").val("");
+  $("#delPageButton").hide();
+
 }
 
 /**
- * Permet de supprimer la page dont l'id est donné
- */
+* Permet de supprimer la page dont l'id est donné
+*/
 function delPage(){
-    if($("#idPage").val() != ""){
-        $delPage = {
-            "id" : $("#idPage").val(),
-        };
+  if($("#idPage").val() != ""){
+    $delPage = {
+      "id" : $("#idPage").val(),
+    };
 
-        if (confirm("Voulez-vous effacer la page ?") === true) {
-            $.ajax({
-                url : 'delPage',
-                type : 'GET',
-                data : $delPage,
-                success : function(){
-                    location.reload(true);
-                },
-                error : function(resultat, statut, erreur){
-                }
-            });
+    if (confirm("Voulez-vous effacer la page ?") === true) {
+      $.ajax({
+        url : 'delPage',
+        type : 'GET',
+        data : $delPage,
+        success : function(){
+          location.reload(true);
+        },
+        error : function(resultat, statut, erreur){
         }
-    } else {
-        alert("L'opération est impossible");
+      });
     }
+  } else {
+    alert("L'opération est impossible");
+  }
 }
